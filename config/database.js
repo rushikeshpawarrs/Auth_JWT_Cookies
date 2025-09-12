8e06e2a3997a718cdc43701c1f936cb38f50345a
Rushikesh Pawar <rushikeshpawarrs@gmail.com>
2025-09-12 11:44:18 +0530

    Authentication using JWT

--- config/database.js
+++ config/database.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
+}
--- index.js
+++ index.js
@@ -5,3 +5,14 @@ require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+require('./config/database').connect();
+
+// Import routes
+const user = require("./routes/user");
+app.use("/api/v1", user);
+
+//activate the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
